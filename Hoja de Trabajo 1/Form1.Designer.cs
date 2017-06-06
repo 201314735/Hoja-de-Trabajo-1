db5ae1d8fc25cfe60c9a6a13759cdda84321fcb1
201314735 <201314735@ingenieria.usac.edu.gt>
2017-06-06 07:58:04 -0600

    5 Vocales

--- Hoja de Trabajo 1/Form1.Designer.cs
+++ Hoja de Trabajo 1/Form1.Designer.cs
@@ -37,6 +37,7 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -115,11 +116,22 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(194, 238);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 39);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "5 Vocales";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(603, 389);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -147,6 +159,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Hoja de Trabajo 1/Form1.cs
+++ Hoja de Trabajo 1/Form1.cs
@@ -41,5 +41,36 @@ namespace Hoja_de_Trabajo_1
 
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string palabra = textBox1.Text;
+            palabra = palabra.ToLower();
+
+            string consignos = "áàäéèëíìïóòöúùuñÁÀÄÉÈËÍÌÏÓÒÖÚÙÜ";
+            string sinsignos = "aaaeeeiiiooouuunAAAEEEIIIOOOUUU";
+            for (int v = 0; v < sinsignos.Length; v++)
+            {
+                string i = consignos.Substring(v, 1);
+                string j = sinsignos.Substring(v, 1);
+                palabra = palabra.Replace(i, j);
+            }
+
+            label5.Text = "";
+
+            if (palabra.Contains("a") && palabra.Contains("e") && palabra.Contains("i") && palabra.Contains("o") && palabra.Contains("u"))
+            {
+
+                label5.Text = "CUMPLE";
+            }
+
+            else { 
+
+                label5.Text = "NO CUMPLE";
+
+            }
+
+
+        }
     }
 }
