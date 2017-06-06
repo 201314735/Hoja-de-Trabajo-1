8cc2bc1bea778cb4e837ee3d7be29c562a59808c
201314735 <201314735@ingenieria.usac.edu.gt>
2017-06-06 07:53:18 -0600

    Calcular Hipotenusa

--- Hoja de Trabajo 1/Form1.cs
+++ Hoja de Trabajo 1/Form1.cs
@@ -16,5 +16,30 @@ namespace Hoja_de_Trabajo_1
         {
             InitializeComponent();
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            double catetoO;
+            double catetoA;
+            double hipotenusa;
+
+            label5.Text ="";
+            hipotenusa = 0;
+
+            catetoO = Convert.ToDouble(textBox1.Text);
+            catetoA = Convert.ToDouble(textBox2.Text);
+
+            hipotenusa = Math.Sqrt((catetoO*catetoO)+(catetoA*catetoA));
+
+            label5.Text = Convert.ToString(hipotenusa);
+
+
+        }
     }
 }
